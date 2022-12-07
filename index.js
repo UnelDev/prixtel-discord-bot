61d7771c67e4d4234c722fc59031d8b77de0a3ec
unel <enee.prog@gmail.com>
2022-12-07 14:32:14 +0100

    added alert backups

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // Require the necessary discord.js classes
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import * as dotenv from 'dotenv';
+import alert from './commands/alert.js';
 import ping from './commands/ping.js';
 import report from './commands/report.js';
 import server from './commands/server.js';
 import user from './commands/user.js';
 import deploy from './deploy.js';
-import alert from './commands/alert.js';
 import prixtelApi from './prixtel-api/index.js';
+import Alert from './prixtel-api/sendAlert.js';
 dotenv.config();
+//deply command
+const pending = [];
 //create a new api
 process.api = new prixtelApi();
+//connected to api
+pending.push(process.api.Connect(process.env.EMAIL, process.env.PASSWORD));
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 process.client = client;
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
@@ -31,7 +37,11 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 
     try {
-        await command.execute(interaction);
+        if (command.data.name == 'alert') {
+            await command.execute(interaction, alertClass)
+        } else {
+            await command.execute(interaction);
+        }
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
@@ -56,13 +66,12 @@ client.commands = new Collection();
 Listcommand.forEach((element) => {
     client.commands.set(element.data.name, element);
 })
-//deply command
-const pending = [];
-//connected to api
-pending.push(process.api.Connect(process.env.EMAIL, process.env.PASSWORD));
 //deploy command
 pending.push(deploy(Listcommand));
 await Promise.all(pending);
+//create alert
+const alertClass = new Alert();
+await alertClass.restore();
 // Log in to Discord with your client's token
 if (process.env.ISDEV == 'true') {
     client.login(process.env.BOT_DEV_TOKEN);
--- prixtel-api/sendAlert.js
+++ prixtel-api/sendAlert.js
@@ -1,21 +1,103 @@
-async function PrixtelAlert(channelid, alert, role, timer) {
-    const channel = await process.client.channels.cache.get(channelid);
-    if (typeof timer != 'number') {
-        timer = 5000;
+import fs from 'node:fs';
+import path from 'node:path';
+export default class Alert {
+    constructor() {
+        this.listAlert = [];
+    }
+    async alert(channelid, alert, role, timer) {
+        //create new alert comp and add in list
+        const id = this.listAlert.length
+        this.listAlert.push(new alertComp(channelid, alert, role, timer, id, await process.api.GetDataConsumme()));
+        //start
+        this.listAlert[id].fetch(this.save, this.removeAlert);
+        this.save();
+        return id;
+    }
+    removeAlert(id) {
+        // delete object
+        delete this.listAlert[id];
+        // delet pointer in array
+        this.listAlert.splice(id, 1);
+        this.save();
+    }
+    async save() {
+        console.log(this.listAlert);
+        // use absolut path for 'save'
+        const saveDir = path.resolve('./save');
+
+        // verify if 'save' exist and create it if dosn't exist
+        fs.access(saveDir, error => {
+            if (error) {
+                // the directory dosn't exist
+                fs.mkdir(saveDir, err => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            }
+            //write 'alertSave.json'
+            fs.writeFile(path.resolve(saveDir, 'alertSave.json'), JSON.stringify(this), err => {
+                if (err) {
+                    throw err;
+                }
+            });
+
+        });
+    }
+    async restore() {
+        // use absolut path for 'save'
+        const saveDir = path.resolve('./save');
+        fs.readFile(path.resolve(saveDir, 'alertSave.json'), 'utf8', (err, data) => {
+            if (err) {
+                console.log('no save detected');
+                return;
+            }
+            this.listAlert = JSON.parse(data).listAlert;
+            this.listAlert.forEach((Element, i) => {
+                this.listAlert[i] = new alertComp(Element.channel, Element.alert, Element.PingRole, Element.timer, i, Element.initial, Element.initialTime);
+                this.listAlert[i].fetch(this.save, this.removeAlert);
+            })
+        });
+    }
+}
+
+class alertComp {
+    constructor(channelid, alert, role, timer, id, initial, initialTime) {
+        this.channel = channelid;
+        this.alert = alert;
+        this.PingRole = role;
+        this.timer = (typeof timer != 'number') ? 5000 : timer;
+        this.id = id;
+        this.initial = initial;
+        // if (typeof initial != 'undefined') {
+        //     this.initial = initial;
+        // } else {
+        //     console.log('is undefined');
+        //     process.api.GetDataConsumme().then((result) => {
+        //         console.log('1');
+        //         this.initial = result;
+        //         resolve();
+        //     });
+        //     console.log(this.initial);
+        // }
+        this.initialTime = initialTime ?? new Date;
     }
 
-    let initial = await process.api.GetDataConsumme();
-    let initialTime = new Date;
-    initial = initial.replace(' Go', '');
-    setInterval(async () => {
-        let newData = await process.api.GetDataConsumme();
-        newData = newData.replace(' Go', '');
-        console.log(parseFloat(initial) + parseFloat(alert), '<=', newData, parseFloat(initial) + parseFloat(alert) <= newData)
-        if (parseFloat(initial) + parseFloat(alert) <= newData) {
-            channel.send(role + ' un palier vien d\'etre déclancher vous avez consomé plus de' + alert + 'Go depuis le ' + initialTime.toLocaleDateString('fr-FR'));
-            initial = newData;
-            initialTime = new Date();
+
+    async fetch(create, remove) {
+        const channel = await process.client.channels.cache.get(this.channelid);
+        if (typeof timer != 'number') {
+            this.timer = 5000;
         }
-    }, timer);
+        this.initial = this.initial.replace(' Go', '');
+        setInterval(async () => {
+            let newData = await process.api.GetDataConsumme();
+            newData = newData.replace(' Go', '');
+            if (parseFloat(this.initial) + parseFloat(this.alert) <= newData) {
+                channel.send(this.PingRole + ' un palier vien d\'etre déclancher vous avez consomé plus de' + this.alert + 'Go depuis le ' + initialTime.toLocaleDateString('fr-FR'));
+                create(this.channelid, this.alert, this.PingRole, this.timer);
+                remove(this.id);
+            }
+        }, this.timer);
+    }
 }
-export default PrixtelAlert;
